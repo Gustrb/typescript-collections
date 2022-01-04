2debfa785bc486249c732f3b8d4a1cf2353db627
Gustrb <gustavoreisbauer@gmail.com>
2022-01-04 13:29:18 -0300

    Fix: Adding dist to the gitignore and removing useless interface exporting

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,5 @@ export * from './array_list/ArrayList';
 export * from './linked_list/LinkedList';
 export * from './stack/Stack';
 
-export * from './interfaces/List';
-export * from './interfaces/Queue';
-export * from './interfaces/Stack';
-
 export * from './exceptions/IndexOutOfBoundsException';
 export * from './exceptions/EmptyStackException';
