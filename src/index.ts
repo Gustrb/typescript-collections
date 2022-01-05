2ef8840754235ce5912811abc69ec798a0ec676b
Gustrb <gustavoreisbauer@gmail.com>
2022-01-05 19:04:18 -0300

    Fix: Adding Queue exports in the main module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './array_list/ArrayList';
 export * from './linked_list/LinkedList';
 export * from './stack/Stack';
+export * from './resizable_array_queue/ResizableArrayQueue';
 
 export * from './exceptions/IndexOutOfBoundsException';
 export * from './exceptions/EmptyStackException';
